73db7a9ea52fba3f4337f2bcbfbb9220c7247b79
Andre Ganske <andre.g.ganske@gmail.com>
2014-12-26 12:19:12 -0200

    iniciando com o angular

--- client/web/app/app.js
+++ client/web/app/app.js
@@ -3,10 +3,26 @@
 // Declare app level module which depends on views, and components
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
+  'ngMaterial',
+
+  'myApp.home',
   'myApp.view2',
+  
   'myApp.version'
-]).
-config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
-}]);
+])/*
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.otherwise({redirectTo: '/home'});
+  controller: 'myAppCtrl'
+}])*/
+.controller('AppCtrl', function($scope) {
+	$scope.pages = [
+		{
+			name 	: 'home',
+			link	: '#/view/home'
+		},
+		{
+			name 	: 'view2',
+			link	: '#/view2'
+		}
+	]
+});
