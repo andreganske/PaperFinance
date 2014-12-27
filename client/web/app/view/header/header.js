0811b311b4f37613ed6f358ed4b00c3802963728
Andre Ganske <andre.g.ganske@gmail.com>
2014-12-27 01:26:49 -0200

    botoes de login e signin no header

--- client/web/app/view/header/header.js
+++ client/web/app/view/header/header.js
@@ -1,14 +1,38 @@
 var app = angular.module('myApp.header', ['ngRoute', 'ngMaterial']);
 
-app.controller('headerCrtl', function($scope) {
-	$scope.pages = [
-		{
-			name 	: 'home',
-			link	: '#/view/home'
-		},
-		{
-			name 	: 'view2',
-			link	: '#/view2'
-		}
-	]
+app.controller('headerCrtl', function($scope, $mdDialog) {
+	$scope.login = 'Log in';
+	$scope.signin = 'Sign in';
+
+
+	$scope.showLogin = function(ev) {
+    var confirm = $mdDialog.confirm()
+      .title('Would you like to delete your debt?')
+      .content('All of the banks have agreed to forgive you your debts.')
+      .ariaLabel('Lucky day')
+      .ok('Please do it!')
+      .cancel('Sounds like a scam')
+      .targetEvent(ev);
+    $mdDialog.show(confirm).then(function() {
+      $scope.alert = 'You decided to get rid of your debt.';
+    }, function() {
+      $scope.alert = 'You decided to keep your debt.';
+    });
+	};
+
+	$scope.showSignin = function(ev) {
+    var confirm = $mdDialog.confirm()
+      .title('Would you like to delete your debt?')
+      .content('All of the banks have agreed to forgive you your debts.')
+      .ariaLabel('Lucky day')
+      .ok('Please do it!')
+      .cancel('Sounds like a scam')
+      .targetEvent(ev);
+    $mdDialog.show(confirm).then(function() {
+      $scope.alert = 'You decided to get rid of your debt.';
+    }, function() {
+      $scope.alert = 'You decided to keep your debt.';
+    });
+	};
+
 });
